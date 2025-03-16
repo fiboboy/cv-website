5918f0ee824044c5c218fa6bb56d7cb257e4a1f2
fiboboy <maodzyu@gmail.com>
2025-03-17 00:46:44 +0700

    deploy_fix

--- web/src/components/ui/timeline/TimelineCard.tsx
+++ web/src/components/ui/timeline/TimelineCard.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { useRef, useState, useEffect } from 'react';
 import { ActivePeriod, ExpandedCard, ProcessedTimelineItem } from './types';
 import { AcademicCapIcon, BriefcaseIcon, PersonalIcon } from './CategoryIcons';
